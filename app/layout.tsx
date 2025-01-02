8bf2cc8c97e3b0d5c082bc54c5bf997c7006512a
Dawid Jabłoński <xederro@gmail.com>
2025-01-02 18:55:05 +0100

    changed data of title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Komputery express",
+  description: "Your one-stop shop for premium computer components and accessories",
 };
 
 const geistSans = Geist({
